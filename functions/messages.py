a0dd3ca5cd4cdc93a4cc272808baa4600a51af49
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-06-26 21:38:01 +1000

    Create messages.py

---
+++ functions/messages.py
@@ -0,0 +1,19 @@
+import logging
+from random import randint
+
+from vkmax.client import MaxClient
+
+_logger = logging.getLogger(__name__)
+
+async def send_message(client: MaxClient, chat_id: int, text: str, notify=True):
+    """Sends message to chat"""
+    await client.invoke_method(64, {
+    "chatId": chat_id,
+    "message": {
+      "text": text,
+      "cid": randint(1750000000000, 2000000000000),
+      "elements": [],
+      "attaches": []
+    },
+    "notify": notify
+  })
