0faeb735b15bec6cad5df172738710bb12d35ee6
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-06-26 17:14:39 +1000

    Update example.py
    added example for create_group method

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
 import asyncio
 import logging
 import sys
+from random import randint
 
 from vkmax.client import MaxClient
-from vkmax.features.messages import ayumax_callback, get_deleted_messages
+from vkmax.features.messages import ayumax_callback
+# from vkmax.features.users import resolve_users, add_to_contacts
+from vkmax.features.groups import create_group
 
 date_format = '%d.%m.%Y %H:%M:%S'
 logging_format = '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s'
@@ -22,13 +25,9 @@ logging.basicConfig(
 async def main():
     client = MaxClient()
     await client.connect()
-    # await client.login_by_token("token") # maybe you have a token ?
-    # get_token = await client.start_sms_login("phone_number") # if you want to login manually
-    # sms_code = int(input("Input SMS code: "))
-    # await client.finish_sms_login(get_token, sms_code)
-    await client.start("phone_number") # logging in via terminal
-    print(await get_deleted_messages(chat_id=123))
-    client.set_callback(ayumax_callback)
+    await client.start("phone_number")
+    await create_group(client, "ЭКСТЕРАГРАМ ОБНОВИЛСЯ!!!", [participant_id])
+    await client.set_callback(ayumax_callback)
     await asyncio.Future()  # run forever
 
 
