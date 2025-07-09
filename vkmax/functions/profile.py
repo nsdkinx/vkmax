4a83d9e51860bd5b54baf755c87d965a696c2621
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-07-09 22:22:27 +1000

    profile update! now you can change your public profile and interact with account privacy settings. + refactoring

--- vkmax/functions/profile.py
+++ vkmax/functions/profile.py
@@ -3,14 +3,18 @@ from vkmax.client import MaxClient
 async def change_online_status_visibility(
         client: MaxClient,
         hidden: bool
-):
-    """Hide or show you last online status"""
+    ):
+
+    """ Hide or show you last online status """
+
     return await client.invoke_method(
-        opcode=22,
-        payload={
-            "settings": {
-                "user": {
-                    "HIDDEN":hidden
+        opcode = 22,
+        payload = {
+            "settings":
+            {
+                "user":
+                {
+                    "HIDDEN": hidden
                 }
             }
         }
@@ -19,30 +23,79 @@ async def change_online_status_visibility(
 async def set_is_findable_by_phone(
         client: MaxClient,
         findable: bool
-):
-    """Changes your profile privacy settings. You can make your profile findable by phone or not."""
+    ):
+
+    """ Changes your profile privacy settings. You can make your profile findable by phone or not. """
+    
     findable = "ALL" if findable else "CONTACTS"
     return await client.invoke_method(
-        opcode=22,
-        payload={
-            "settings": {
-                "user": {
-                    "SEARCH_BY_PHONE":findable
+        opcode = 22,
+        payload = {
+            "settings":
+            {
+                "user":
+                {
+                    "SEARCH_BY_PHONE": findable
                 }
             }
         }
     )
 
-async def set_calls_privacy(client: MaxClient, can_be_called: bool):
-    """You can enable or disable calls for everyone."""
+async def set_calls_privacy(
+        client: MaxClient,
+        can_be_called: bool
+    ):
+
+    """ You can enable or disable calls for everyone. """
+
     findable = "ALL" if can_be_called else "CONTACTS"
     return await client.invoke_method(
-        opcode=22,
-        payload={
-            "settings": {
-                "user":{
-                    "INCOMING_CALL":findable
+        opcode = 22,
+        payload = {
+            "settings":
+            {
+                "user":
+                {
+                    "INCOMING_CALL": findable
                 }
             }
         }
     )
+
+async def invite_privacy(
+        client: MaxClient,
+        invitable:  bool
+    ):
+
+    """ Changes privacy settings\nMakes possible or impossible inviting you to other chats """
+
+    invitable = "ALL" if invitable else "CONTACTS"
+    return await client.invoke_method(
+        opcode = 22,
+        payload = {
+            "settings": {
+                "user":
+                {
+                    "CHATS_INVITE": invitable
+                }
+            }
+        }
+    )
+
+async def change_profile(
+        client: MaxClient,
+        first_name = None,
+        last_name = None,
+        bio = None
+    ):
+
+    """ Changes your public profile\nRecommended to use with all available options"""
+
+    return await client.invoke_method(
+        opcode = 16,
+        payload = {
+            "firstName": first_name,
+            "lastName": last_name,
+            "description": bio}
+    )
+    
