51fc28ce64c02c24be51029438dca1d61bffd44f
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-07-09 21:59:37 +1000

    messages update! now you can reply to messages and pin them in your chats ;)

--- vkmax/functions/messages.py
+++ vkmax/functions/messages.py
@@ -7,10 +7,10 @@ async def send_message(
         text: str,
         notify: bool = True
 ):
-    """Sends message to specified chat"""
+    """ Sends message to specified chat """
     return await client.invoke_method(
-        opcode=64,
-        payload={
+        opcode = 64,
+        payload = {
                 "chatId": chat_id,
                 "message": {
                 "text": text,
@@ -23,15 +23,15 @@ async def send_message(
     )
 
 async def edit_message(client: MaxClient, chat_id: int, message_id: int, text: str):
-    """Edits the specified message"""
+    """ Edits the specified message """
     return await client.invoke_method(
-        opcode=67,
-        payload={
-            "chatId":chat_id,
-            "messageId":str(message_id),
-            "text":text,
-            "elements":[],
-            "attachments":[]
+        opcode = 67,
+        payload = {
+            "chatId": chat_id,
+            "messageId": str(message_id),
+            "text": text,
+            "elements": [],
+            "attachments": []
         }
     )
 
@@ -41,12 +41,55 @@ async def delete_message(
         message_ids: list,
         delete_for_me: bool = False
 ):
-    """Deletes the specified message"""
+    """ Deletes the specified message """
     return await client.invoke_method(
-        opcode=66,
-        payload={
-            "chatId":chat_id,
-            "messageIds":message_ids,
+        opcode = 66,
+        payload = {
+            "chatId": chat_id,
+            "messageIds": message_ids,
             "forMe": delete_for_me
         }
     )
+
+async def pin_message(
+        client: MaxClient,
+        chat_id: int,
+        message_id: int,
+        notify = False
+    ):
+    """ Pins message in the chat """
+    return await client.invoke_method(
+        opcode = 55,
+        payload = {
+            "chatId": chat_id,
+            "notifyPin": notify,
+            "pinMessageId": str(message_id)
+            }
+    )
+
+async def reply_message(
+        client: MaxClient,
+        chat_id: int,
+        text: str,
+        reply_to_message_id: int,
+        notify = True
+    ):
+    """ Replies to message in the chat """
+    return await client.invoke_method(
+        opcode = 64,
+        payload = {
+            "chatId": chat_id,
+            "message": {
+                "text": text,
+                "cid": randint(1750000000000, 2000000000000),
+                "elements": [],
+                "link": {
+                    "type": "REPLY",
+                    "messageId": str(reply_to_message_id)
+                    },
+                "attaches": []
+                },
+            "notify": notify
+            }
+    )
+
