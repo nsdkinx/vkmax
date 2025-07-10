88aa9cc57f41be9ef733c7f5c4f83ea1be4d0ad0
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-07-10 16:17:55 +1000

    only refactoring. unified code style 🤗

--- vkmax/functions/users.py
+++ vkmax/functions/users.py
@@ -3,14 +3,40 @@ from vkmax.client import MaxClient
 
 _logger = logging.getLogger(__name__)
 
-async def resolve_users(client: MaxClient, user_id: list):
+async def resolve_users(
+        client: MaxClient,
+        user_id: list
+    ):
+
     """ Resolving users via userid"""
-    return await client.invoke_method(32, {"contactIds":user_id})
+
+    return await client.invoke_method(
+        opcode = 32,
+        payload = {
+            "contactIds":user_id
+            }
+        )
 
 async def add_to_contacts(client: MaxClient, user_id: int):
+
     """ Adding user to contacts via userid """
-    return await client.invoke_method(34, {"contactId":user_id,"action":"ADD"})
+
+    return await client.invoke_method(
+        opcode = 34,
+        payload = {
+            "contactId":user_id,
+            "action":"ADD"
+            }
+        )
 
 async def ban(client: MaxClient, user_id: int):
+
     """ Banhammer to user's head. """
-    return await client.invoke_method(34, {"contactId":user_id,"action":"BLOCK"})
+
+    return await client.invoke_method(
+        opcode = 34,
+        payload = {
+            "contactId":user_id,
+            "action":"BLOCK"
+            }
+        )
