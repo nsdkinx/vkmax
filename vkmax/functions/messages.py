0c35244a7ced7e8094a16b6ba7154c35ab7e293f
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-08-30 01:24:59 +1000

    now you can send photos!
    snd nds

--- vkmax/functions/messages.py
+++ vkmax/functions/messages.py
@@ -105,3 +105,71 @@ async def reply_message(
             "notify": notify
         }
     )
+
+async def send_photo(
+        client: MaxClient,
+        chat_id: int,
+        image_path: str,
+        caption: str,
+        notify: bool = True
+    ):
+
+    """ Sends photo to specified chat """
+
+    import requests
+
+    photo_token = await client.invoke_method(
+        opcode = 80,
+        payload = {
+            "count": 1
+        }
+    )    
+
+    url = photo_token["payload"]["url"]
+
+    params = {
+        'apiToken': url.split('apiToken=')[1]
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
+        'Accept': '*/*',
+        'Origin': 'https://web.max.ru',
+        'Referer': 'https://web.max.ru/',
+        'Sec-Fetch-Site': 'cross-site',
+        'Sec-Fetch-Mode': 'cors',
+        'Accept-Language': 'ru-RU,ru;q=0.9',
+    }
+
+    try:
+        with open(image_path, 'rb') as image_file:
+            files = {
+                'file': ('image.jpg', image_file, 'image/jpeg') # that's not matter don't think about that lol
+            }
+
+            uploaded_photo = requests.post(url, params=params, headers=headers, files=files)
+            uploaded_photo = uploaded_photo.json()
+
+    except FileNotFoundError:
+        print(f"Error: Make sure '{image_path}' is in the same directory.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    await client.invoke_method(
+        opcode=64,
+        payload = {
+            "chatId": chat_id,
+            "message": {
+                "text": caption,
+                "cid": randint(1750000000000, 2000000000000),
+                "elements": [],
+                "attaches": [
+                    {
+                        "_type": "PHOTO",
+                        "photoToken": list(uploaded_photo['photos'].values())[0]['token']
+                    }
+                ]
+            },
+            "notify": notify
+        }
+    )
